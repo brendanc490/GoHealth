89484f32c9801645a09465e5c1b0ca01d1f212e1
Brendan Collins <bcollin4@umd.edu>
2023-11-05 12:10:25 -0500

    Added delete meal functionality

--- user.js
+++ user.js
@@ -28,7 +28,7 @@ const user = {
             }
         },
         "daysEntered": {
-            "10/20/2023":{
+            "10/18/2023":{
                 "Breakfast": {
                     "eggs": "3",
                     "bread": "2",
@@ -46,6 +46,437 @@ const user = {
                     "olive oil": "1/2"
                 },
                 "totalCalories": 2267
+            },
+            "10/17/2023":{
+                "Breakfast": {
+                    "eggs": "2",
+                    "steak": "1",
+                    "banana": "1",
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1628
+            },
+            "10/16/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1827
+            },
+            "10/15/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "10/14/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "10/13/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1827
+            },
+            "10/12/2023":{
+                "Breakfast": {
+                    "eggs": "2",
+                    "steak": "1",
+                    "banana": "1",
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1628
+            },
+            "10/11/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "10/10/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "10/9/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1827
+            },
+            "10/8/2023":{
+                "Breakfast": {
+                    "eggs": "2",
+                    "steak": "1",
+                    "banana": "1",
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1628
+            },
+            "10/7/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1827
+            },
+            "10/6/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "10/5/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "10/4/2023":{
+                "Breakfast": {
+                    "eggs": "2",
+                    "steak": "1",
+                    "banana": "1",
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1628
+            },
+            "10/3/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "10/2/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1827
+            },
+            "10/1/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "9/30/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "9/29/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "9/28/2023":{
+                "Breakfast": {
+                    "eggs": "2",
+                    "steak": "1",
+                    "banana": "1",
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1628
+            },
+            "9/27/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "9/26/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "9/25/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "steak": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 2267
+            },
+            "9/24/2023":{
+                "Breakfast": {
+                    "eggs": "3",
+                    "bread": "2",
+                    "banana": "1",
+                },
+                "Lunch": {
+                    "salmon": "1",
+                    "white rice": "1",
+                    "carrots": "3"
+                },
+                "Dinner": {
+                    "chicken breast": "1",
+                    "white rice": "1",
+                    "broccoli": "3",
+                    "olive oil": "1/2"
+                },
+                "totalCalories": 1827
             }
         }
     },
